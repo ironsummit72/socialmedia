71e7a1ac236d208e34e50a62d55ae1af395ecc1b
ironsummit72 <ironsumit72@gmail.com>
2024-02-02 22:50:52 +0530

    shuffeling the finduser array

--- routes/findusers.js
+++ routes/findusers.js
@@ -1,9 +1,11 @@
 import  { Router } from "express";
 const router=Router();
 import userModel from "../db/models/user.js";
+import shuffle from "../utils/shuffle.js";
 
 router.get('/',isloggedIn,async (req,res)=>{
     const users=await userModel.find()
+	shuffle(users);
 res.render('findusers',{users})
 })
 
