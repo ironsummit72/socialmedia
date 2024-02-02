12c5d310ff93a7f50bb972a8d1fb8a05cc070188
ironsummit72 <ironsumit72@gmail.com>
2024-02-02 22:50:34 +0530

    shuffleing the explore array

--- routes/explore.js
+++ routes/explore.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import postModel from "../db/models/post.js"
+import shuffle from "../utils/shuffle.js";
 const router=Router();
-
 
 router.get('/',isloggedIn, async function(req, res){
     const postData=await postModel.find({}).populate('user');
-	
-	console.log(postData);
+    shuffle(postData)
 	res.render('explore',{postData})
 })
 
