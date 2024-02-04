2c2b98bd49ef2b522cdc2fa55e1186b191f1952e
ironsummit72 <ironsumit72@gmail.com>
2024-02-04 20:01:33 +0530

    now hashtags are available in caption

---
+++ public/javascripts/caption.js
@@ -0,0 +1,12 @@
+const getCaptionList = document.querySelectorAll('.getcaption')
+const captionList = document.querySelectorAll('.caption')
+const reUrl = /#(\w+)/gi
+const captionTextList = getCaptionList.forEach((caption, index) => {
+	const newCaption = document.createElement('div')
+	console.log(caption.textContent, index)
+	newCaption.innerHTML = linkify(caption.textContent)
+	captionList[index].appendChild(newCaption)
+})
+function linkify(text) {
+	return text.replace(reUrl, (url) => `<a style="color:blue;" href="tags/${url.slice(1)}" target="_blank">${url}</a>`)
+}
