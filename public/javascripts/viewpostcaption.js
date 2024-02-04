0cb393d260de319ce9d062cd91b1f17ef93a7db8
ironsummit72 <ironsumit72@gmail.com>
2024-02-04 23:38:51 +0530

    hashtags are also available in viewposts

---
+++ public/javascripts/viewpostcaption.js
@@ -0,0 +1,11 @@
+const getCaptionList = document.querySelectorAll('.getcaption')
+const captionList = document.querySelectorAll('.caption')
+const reUrl = /#(\w+)/gi
+const captionTextList = getCaptionList.forEach((caption, index) => {
+	const newCaption = document.createElement('div')
+	newCaption.innerHTML = linkify(caption.textContent)
+	captionList[index].appendChild(newCaption)
+})
+function linkify(text) {
+	return text.replace(reUrl, (url) => `<a style="color:blue;" href="../tags/${url.slice(1)}">${url}</a>`)
+}
