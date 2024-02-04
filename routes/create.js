3ff77c8cb07fe5dac2088f7eda6640b116421965
ironsummit72 <ironsumit72@gmail.com>
2024-02-04 14:25:40 +0530

    hastag logic added for creating tags

--- routes/create.js
+++ routes/create.js
@@ -3,6 +3,7 @@ import express,{Router} from 'express'
 import multer,{diskStorage} from 'multer'
 import userModel from '../db/models/user.js'
 import postModel from '../db/models/post.js'
+import tagsModel from '../db/models/tags.js'
 const router = Router()
 router.use(isloggedIn)
 
@@ -38,12 +39,31 @@ router.post('/post', upload.array('posts', 6),async function (req, res) {
 
 	const {username}=req.user
 	const {caption}=req.body
+    const tags=caption.split('#').slice(1)
+
+   
 	if(!req.file)
 	{
 		let userData = await userModel.findOne({ username })
 		let postData = await postModel.create({ user: userData._id,caption,contents:req.files})
 		userData.posts.push(postData._id)
 		await userData.save()
+		tags.forEach(async(items)=>{
+			const tagsData=await tagsModel.findOne({tagName:items.trim()})
+			if(tagsData===null)
+			{
+				const createdData=await tagsModel.create({tagName:items.trim()})
+				createdData.posts.push(postData._id)
+				await createdData.save()
+				console.log('created Data ',createdData);
+			}else{
+				const tagData=await tagsModel.findOne({tagName:items.trim()})
+				 tagData.posts.push(postData._id)
+				 await tagData.save()
+				console.log('exists already');
+				
+			}
+		})
 		// res.send('postcreated successfully')
 		res.redirect('/')
 		
