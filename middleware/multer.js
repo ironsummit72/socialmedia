c4e881869e7204a49b8846b6457516ac46a6eb5e
ironsummit72 <ironsumit72@gmail.com>
2024-02-08 00:10:28 +0530

    added multer in a middleware directory

---
+++ middleware/multer.js
@@ -0,0 +1,41 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      if(file.fieldname==='posts'){
+        const [type, extension] = file.mimetype.split('/')
+        if (type === 'image') {
+			cb(null, `uploads/${file.fieldname}/images`)
+		} else if (type === 'video') {
+			cb(null, `uploads/${file.fieldname}/videos`)
+		}
+
+    }else{
+
+        if (!fs.existsSync("uploads/" + file.fieldname)) {
+            fs.mkdirSync("uploads/" + file.fieldname);
+            cb(null, `./uploads/${file.fieldname}`);
+        } else {
+            cb(null, `./uploads/${file.fieldname}`);
+        }
+    }
+    },
+  filename: function (req, file, cb) {
+    if(file.fieldname==='posts')
+    {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+		const [type, extension] = file.mimetype.split('/')
+		cb(null, type + '-' + req.user.username+'-' + uniqueSuffix+ path.extname(file.originalname))
+    }else{
+
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(
+            null,
+            file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+            );
+        }
+  },
+});
+const upload = multer({ storage: storage });
+export default upload;
