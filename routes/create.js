1ced7674ae89c83b810eeef582a627bfe4a4c466
ironsummit72 <ironsumit72@gmail.com>
2024-02-08 00:09:47 +0530

    removed multer from here to the middleware directory for reusability

--- routes/create.js
+++ routes/create.js
@@ -1,47 +1,21 @@
 
-import express,{Router} from 'express'
-import multer,{diskStorage} from 'multer'
+import {Router} from 'express'
 import userModel from '../db/models/user.js'
 import postModel from '../db/models/post.js'
 import tagsModel from '../db/models/tags.js'
+import upload from '../middleware/multer.js'
 const router = Router()
 router.use(isloggedIn)
 
-const storage = diskStorage({
-	destination: function (req, file, cb) {
-		
-		// eslint-disable-next-line no-unused-vars
-		const [type, extension] = file.mimetype.split('/')
-		if (type === 'image') {
-			cb(null, 'uploads/posts/images')
-		} else if (type === 'video') {
-			cb(null, 'uploads/posts/videos')
-		}
-	},
-	filename: function (req, file, cb) {
-		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
-		const [type, extension] = file.mimetype.split('/')
-		cb(null, type + '-' + req.user.username + uniqueSuffix + '.' + extension)
-	},
-})
 router.get('/post', function (req, res) {
 	res.render('createpost', { username: req.user.username })
 })
-const upload = multer({ storage: storage })
-
 // _____________________________ post requests____________________
 
-
-
-
 router.post('/post', upload.array('posts', 6),async function (req, res) {
-
-
 	const {username}=req.user
 	const {caption}=req.body
     const tags=caption.split('#').slice(1)
-
-   
 	if(!req.file)
 	{
 		let userData = await userModel.findOne({ username })
@@ -64,14 +38,8 @@ router.post('/post', upload.array('posts', 6),async function (req, res) {
 				
 			}
 		})
-		// res.send('postcreated successfully')
 		res.redirect('/')
-		
 	}
-
-
-
-
 })
 
 function isloggedIn(req, res, next) {
