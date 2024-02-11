f1ea9bc3c5491a85311eeb65f2d9e88024706b1d
ironsummit72 <ironsumit72@gmail.com>
2024-02-12 00:38:55 +0530

    stories route added

---
+++ routes/stories.js
@@ -0,0 +1,27 @@
+import {Router} from 'express'
+import userModel from '../db/models/user.js'
+import storyModel from '../db/models/story.js'
+const router = Router()
+router.get('/', async function (req, res) {
+	const {username} = req.user || {}
+	const userData = await userModel.findOne({username})
+	const {_id} = userData || {}
+	const storyData = await storyModel.find({user: _id})
+	if (storyData.length > 0) {
+		res.render('stories', {storyData})
+	} else {
+		res.redirect('../')
+	}
+})
+router.get('/:username', async function (req, res) {
+	const {username} = req.params
+	const userData = await userModel.findOne({username})
+	const {_id} = userData || {}
+	const storyData = await storyModel.find({user: _id})
+	if (storyData.length > 0) {
+		res.render('stories', {storyData})
+	} else {
+		res.redirect(`../profile/${username}`)
+	}
+});
+export default router
