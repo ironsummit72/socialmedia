a2990b73737086f834f8ad06ccf9ced61b08b461
ironsummit72 <ironsumit72@gmail.com>
2024-02-12 00:34:37 +0530

    a new story db model created for stories

---
+++ db/models/story.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+const storySchema = new mongoose.Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'users',
+		},
+		caption: String,
+		content: {},
+		views: [{
+			type: mongoose.Schema.Types.ObjectId,
+			ref:'users'
+		}]
+	},
+	{timestamps: true},
+)
+storySchema.index({createdAt: 1}, {expireAfterSeconds: 3600})
+const storyModel = mongoose.model('story', storySchema)
+export default storyModel
