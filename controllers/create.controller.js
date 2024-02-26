bf1059d2058e2c2377c68d380b392988f60c17f9
ironsummit72 <ironsumit72@gmail.com>
2024-02-26 20:57:06 +0530

    Refactor: Move creation-related route handling to controller
    Moved the following functions from create.routes.js to create.controller.js:
    - renderCreatePostPage
    - renderCreateStoryPage
    - uploadPost
    - uploadStory
    
    This change enhances code organization by relocating route handling logic to the controller. Each function now resides in the appropriate file for better separation of concerns.

---
+++ controllers/create.controller.js
@@ -0,0 +1,47 @@
+import userModel from '../db/models/user.model.js'
+import postModel from '../db/models/post.model.js'
+import tagsModel from '../db/models/tags.model.js'
+import storyModel from '../db/models/story.model.js'
+
+function renderCreatePostPage(req, res) {
+	res.render('createpost', {username: req.user.username})
+}
+function renderCreateStoryPage(req, res) {
+	res.render('createstory', {username: req.user.username})
+}
+
+async function uploadPost(req, res) {
+	const {username} = req.user
+	const {caption} = req.body
+	const tags = caption.split('#').slice(1)
+	if (!req.file) {
+		let userData = await userModel.findOne({username})
+		let postData = await postModel.create({user: userData._id, caption, contents: req.files})
+		userData.posts.push(postData._id)
+		await userData.save()
+		tags.forEach(async (items) => {
+			const tagsData = await tagsModel.findOne({tagName: items.trim()})
+			if (tagsData === null) {
+				const createdData = await tagsModel.create({tagName: items.trim()})
+				createdData.posts.push(postData._id)
+				await createdData.save()
+				console.log('created Data ', createdData)
+			} else {
+				const tagData = await tagsModel.findOne({tagName: items.trim()})
+				tagData.posts.push(postData._id)
+				await tagData.save()
+				console.log('exists already')
+			}
+		})
+		res.redirect('/')
+	}
+}
+
+async function uploadStory(req, res) {
+	const {username} = req.user
+	const userData = await userModel.findOne({username})
+	const storyData = await storyModel.create({user: userData._id, caption: req.body.caption, content: req.file})
+	res.send(storyData)
+}
+
+export {renderCreatePostPage, renderCreateStoryPage, uploadPost, uploadStory}
--- routes/create.routes.js
+++ routes/create.routes.js
@@ -1,62 +1,14 @@
-
 import {Router} from 'express'
-import userModel from '../db/models/user.model.js'
-import postModel from '../db/models/post.model.js'
-import tagsModel from '../db/models/tags.model.js'
-import storyModel from '../db/models/story.model.js'
+import {renderCreatePostPage, renderCreateStoryPage, uploadPost, uploadStory} from '../controllers/create.controller.js'
 import upload from '../middleware/multer.js'
+import isloggedIn from '../middleware/auth.middleware.js'
 const router = Router()
 router.use(isloggedIn)
 
-router.get('/post', function (req, res) {
-	res.render('createpost', { username: req.user.username })
-})
-router.get('/story', function (req, res) {
-	res.render('createstory', { username: req.user.username })
-})
+router.get('/post', renderCreatePostPage)
+router.get('/story', renderCreateStoryPage)
 // _____________________________ post requests____________________
+router.post('/post', upload.array('posts', 6), uploadPost)
+router.post('/story', upload.single('stories'), uploadStory)
 
-router.post('/post', upload.array('posts', 6),async function (req, res) {
-	const {username}=req.user
-	const {caption}=req.body
-    const tags=caption.split('#').slice(1)
-	if(!req.file)
-	{
-		let userData = await userModel.findOne({ username })
-		let postData = await postModel.create({ user: userData._id,caption,contents:req.files})
-		userData.posts.push(postData._id)
-		await userData.save()
-		tags.forEach(async(items)=>{
-			const tagsData=await tagsModel.findOne({tagName:items.trim()})
-			if(tagsData===null)
-			{
-				const createdData=await tagsModel.create({tagName:items.trim()})
-				createdData.posts.push(postData._id)
-				await createdData.save()
-				console.log('created Data ',createdData);
-			}else{
-				const tagData=await tagsModel.findOne({tagName:items.trim()})
-				 tagData.posts.push(postData._id)
-				 await tagData.save()
-				console.log('exists already');
-				
-			}
-		})
-		res.redirect('/')
-	}
-})
-
-router.post('/story',upload.single('stories'),async function(req, res){
-const {username}=req.user
-const  userData = await userModel.findOne({ username })
-const storyData=await storyModel.create({user:userData._id,caption:req.body.caption,content:req.file})
-res.send(storyData)
-});
-function isloggedIn(req, res, next) {
-	if (req.user) {
-		next()
-	} else {
-		res.redirect('/login')
-	}
-}
 export default router
