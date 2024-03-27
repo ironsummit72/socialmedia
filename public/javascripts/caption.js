bcb2e7b324635cfb2753605ccb7bed42be469307
ironsummit72 <ironsumit72@gmail.com>
2024-03-27 17:07:01 +0530

    now @user feature works

--- public/javascripts/caption.js
+++ public/javascripts/caption.js
@@ -1,11 +1,12 @@
 const getCaptionList = document.querySelectorAll('.getcaption')
 const captionList = document.querySelectorAll('.caption')
-const reUrl = /#(\w+)/gi
+const reUrlhastag = /#(\w+)/gi
+const reUrlusertag = /@(\w+)/gi
 const captionTextList = getCaptionList.forEach((caption, index) => {
 	const newCaption = document.createElement('div')
 	newCaption.innerHTML = linkify(caption.textContent)
 	captionList[index].appendChild(newCaption)
 })
 function linkify(text) {
-	return text.replace(reUrl, (url) => `<a style="color:blue;" href="tags/${url.slice(1)}">${url}</a>`)
+	return text.replace(reUrlhastag, (url) => `<a style="color:blue;" href="tags/${url.slice(1)}">${url}</a>`)+ text.replace(reUrlusertag, (url) => `<a style="color:blue;" href="profile/${url.slice(1)}">${url}</a>`)
 }
